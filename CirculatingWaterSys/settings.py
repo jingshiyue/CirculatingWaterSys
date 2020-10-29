0c09c48745df60d52499104c639efbbff28f830a
zcl <173302591@qq.com>
2020-10-29 22:40:58 +0800

    2020-10-29 update at home

--- CirculatingWaterSys/settings.py
+++ CirculatingWaterSys/settings.py
@@ -111,14 +111,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-"DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.AutoSchema",
-'DEFAULT_AUTHENTICATION_CLASSES':(
-    'rest_framework.authentication.BasicAuthentication',
-    'rest_framework.authentication.SessionAuthentication',
-    'rest_framework_jwt.authentication.JSONWebTokenAuthentication', #JWT
-    ),
-# 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-# 'PAGE_SIZE': 10,  后头自定义了页分类
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication', #JWT
+        ),
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,  后头自定义了页分类
 }
 
 # Internationalization
